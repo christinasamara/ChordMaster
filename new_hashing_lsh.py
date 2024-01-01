83cca3d9452e2bcf5c1347b9d1175d5f707d517d
christinasamara <christinaesamara@gmail.com>
2024-01-01 18:47:38 +0200

    1/1/24

--- new_hashing_lsh.py
+++ new_hashing_lsh.py
@@ -4,47 +4,50 @@ from sklearn.feature_extraction import text
 from sklearn.neighbors import NearestNeighbors
 from lsh import lsh
 import pandas as pd
+import hashlib
 
 my_stop_words = ["University", "of", "Institute"]
 
-def hash_tfidf_matrix(X_tfidf):
-    # Convert the sparse matrix to a dense array before hashing
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def hash_tfidf_string(tfidf_string):
+    tfidf_vectorizer = TfidfVectorizer(
+        analyzer='char_wb',
+        ngram_range=(5, 6),
+        min_df=0,
+        stop_words=my_stop_words
+    )
+    X_tfidf = tfidf_vectorizer.fit_transform([tfidf_string])
     X_array = X_tfidf.toarray()
 
-    # Hash the entire array
     hashed_value = hash(X_array.tobytes())
 
     return hashed_value
 
-def lsh_with_chord(df, chord_ring_size=16):
-    # Vectorize education strings
-    tfidf_vectorizer = TfidfVectorizer(
-        analyzer='char_wb',
-        ngram_range=(4, 5),
-        min_df=0,
-        stop_words=my_stop_words
-    )
-    chord_keys = []
+def lsh_with_chord_single_string(input_string, chord_ring_size=16):
+    hashed_education = hash_tfidf_string(input_string)
+    chord_key = hashed_education % chord_ring_size
 
-    for index, row in df.iterrows():
-        X_tfidf = tfidf_vectorizer.fit_transform([row['EDUCATION']])
+    return chord_key
 
-        # Hash the TF-IDF matrix into a single integer
-        hashed_education = hash_tfidf_matrix(X_tfidf)
+def sort_characters(text):
+    # Remove whitespaces and single quotes
+    cleaned_text = text.replace(' ', '').replace("'", '')
+    sorted_text = ''.join(sorted(cleaned_text))
+    return sorted_text
 
-        # Map the hashed value to the Chord ring space
-        chord_key = hashed_education % chord_ring_size
+def sha256_hash(input_string):
+    sha256 = hashlib.sha256()
+    new_string = sort_characters(input_string)
+    sha256.update(new_string.encode())
+    hashed_value = int(sha256.hexdigest(), 16)
+    return hashed_value
 
-        chord_keys.append(chord_key)
+if __name__ == "__main__":
+    df = pd.read_csv("data.csv")
+    df_education = df['EDUCATION']
+    for i in df_education:
+        chord_key = sha256_hash(i) % 16
+        print(f"Chord key for '{i}': {chord_key}")
 
-    # Add the Chord keys as a new column to the DataFrame
-    df['Chord_Key'] = chord_keys
-
-    return df
-
-# Example usage:
-df = pd.read_csv("data.csv")
-df_education = df[['EDUCATION']]
-
-result_df = lsh_with_chord(df_education)
-result_df.to_csv("result_data.csv", index=False)
+# for hashing: firstly sort the string and then hash it with sha256
