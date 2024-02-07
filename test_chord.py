ab970d3dc80102833a48380b4a6a712246dcda03
christinasamara <christinaesamara@gmail.com>
2024-02-07 21:47:44 +0200

    7.2

--- test_chord.py
+++ test_chord.py
@@ -14,25 +14,26 @@ delete_times = []
 lookup_times = []
 
 nodes = [Node(i) for i in range(SIZE)]
+
+# First node joining
 start = timer()
 nodes[0].join(nodes[0])
 
+# Data into Chord
 with open("data.csv", 'r') as file:
     for row in file:
         nodes[0].insert_data(row.strip())
 end = timer()
 join_times.append(end-start)
 
-# PRINT ALL NODE.DATA LENGTHS
-# for node in nodes:
-#     print(len(node.data), end=" ")
-# print()
-
+# Other nodes joining - data is being distributed 
 for i in range(1, SIZE):
     start = timer()
     nodes[0].join(nodes[i])
     end = timer()
     join_times.append(end-start)
+
+
 nodes[0].visualize_chord_ring()
 
 nodes[5].delete()
@@ -40,6 +41,10 @@ nodes[5].delete()
 
 
 nodes[0].visualize_chord_ring()
+
+nodes[10].delete()
+nodes[0].visualize_chord_ring()
+
 
 # for i in range(0, SIZE):
 #     for j in range(0, SIZE):
@@ -49,7 +54,7 @@ nodes[0].visualize_chord_ring()
 #         lookup_times.append(end-start)
 
 
-# for i in range(0, SIZE):
+# for i in range(0, SIZE/2):
 #     start = timer()
 #     nodes[i].delete()
 #     end = timer()
@@ -64,10 +69,6 @@ nodes[0].visualize_chord_ring()
 # print("delete_times", delete_times)
 # print("lookup_times", statistics.mean(lookup_times))
 
-# for node in nodes:
-#     print(len(node.data), end=" ")
-# print()
-
 # result = nodes[1].search_education("Massachusetts Institute of Technology", 0)
 # print(result)
 # 
@@ -75,9 +76,14 @@ nodes[0].visualize_chord_ring()
 
 
 
-results= nodes[1].search_scientist("Blum")
-for scientist in results:
-    print("Name: ", scientist)
-    print("Alma Mater: ", results[scientist]["alma mater"])
-    print("Awards: ", results[scientist]["awards"])
-    print()
+# results= nodes[1].search_scientist("Blum")
+# for scientist in results:
+#     print("Name: ", scientist)
+#     print("Alma Mater: ", results[scientist]["alma mater"])
+#     print("Awards: ", results[scientist]["awards"])
+#     print()
+
+# PRINT ALL NODE.DATA LENGTHS
+# for node in nodes:
+#     print(len(node.data), end=" ")
+# print()
